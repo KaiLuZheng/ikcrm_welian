3135eb13aa05ad3f51b34f37bdc484790954e2ee
KaiLuZheng <lumen_o@126.com>
2018-10-18 14:11:29 +0800

    add labels

---
+++ labels.py
@@ -0,0 +1,17 @@
+#-*- coding: utf8 -*-
+
+
+
+
+
+
+project_labels = [  'phone_number', 
+                    'created_time', 
+                    'company', 
+                    'customer.name', 
+                    'project',
+                    'user.name',
+                    'status',
+                    'updated_at',
+                    'lastest.content'
+]
